b5524203911fd76c5ed0d25b972bead0f890c466
Kai Wen Chen <chenkai@us.ibm.com>
2016-07-07 18:05:23 -0400

    Sprint 1 day 1 commit
    - commented out navbar.js and reverted back to html nav in each page
    - maintained previous css design

--- navBar.js
+++ navBar.js
@@ -1,5 +1,7 @@
 /**
  * Created by KWChen on 7/5/16.
+ * This script manages the navigation bar of the site
+ * currently not used
  */
 document.getElementById("navbar").innerHTML =
     // '<ul>'+
@@ -10,15 +12,33 @@ document.getElementById("navbar").innerHTML =
 
     '<ul>' +
         '<li><a href = index.html>Home</a></li>'+
-            '<li><a href = about.html>About</a>' +
-                '<ul>'+
-                    '<li><a href = MissionAndVision.html>Our Mission and Vision</a></li>'+
-                    '<li><a href = MeetOurTeam.html>Meet Our Team</a></li>'+
-                    '<li><a href = #> something 3</a></li>'+
-                '</ul>'+
-            '</li>'+
+        '<li><a href = about.html>About</a>'
+        +
+            '<ul>'
+            +
+                '<li><a href = missionAndVision.html>Our Mission and Vision</a></li>'
+                +
+                '<li><a href = team.html>Meet Our Team</a></li>'
+                +
+                // '<li><a href = #> something 3</a></li>'
+                // +
+            '</ul>'
+            +
+        '</li>'
+        +
 
-        '<li><a href = activities.html>Activities</a></li>'+
+        '<li><a href = activities.html>Activities</a>'
+
+        +
+            '<ul>'
+            +
+                '<li><a href= pastActivities.html> Past Activities</a></li>'
+            +
+
+            '</ul>'
+        +
+
+        '</li>'+
 
         '<li><a href = contact.html>Contact us</a></li>'+
 
